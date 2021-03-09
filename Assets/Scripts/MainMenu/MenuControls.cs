91daed7fe4cbde54d9fa81a555cab1f13e6a9194
Ykisl <yakhs2014@mail.ru>
2021-03-09 23:18:22 +0700

    strange Update

---
+++ Assets/Scripts/MainMenu/MenuControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour
+{
+    MenuMusicManager musicManager;
+
+    public GameObject[] btns;
+
+    public GameObject[] darkModeObj;
+
+    bool darkMode = false;
+
+
+    void Start()
+    {
+        musicManager = GetComponent<MenuMusicManager>();
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Delete) && darkMode == false)
+        {
+            darkMode = true;
+
+            musicManager.SetExtendAudio();
+
+            foreach(GameObject go in btns)
+            {
+                go.SetActive(false);
+            }
+
+            foreach (GameObject go in darkModeObj)
+            {
+                go.SetActive(true);
+            }
+        }
+
+    }
+}
